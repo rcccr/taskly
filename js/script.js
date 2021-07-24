2137d82ab69a74426a96c2629de22f2307899ec4
pradow <pradochris@protonmail.com>
2021-07-24 10:16:07 -0700

    add functions to create list item and to delete list item

--- js/script.js
+++ js/script.js
@@ -2,15 +2,27 @@ const listInput = document.querySelector("#listInput");
 const addButton = document.querySelector("#addButton");
 const list = document.querySelector("#list");
 
+function deleteItem(e) {
+  const liToDelete = e.currentTarget.parentNode;
+
+  list.removeChild(liToDelete);
+}
+
 function addItem() {
-  const inputValue = listInput.value;
-  const li = document.createElement("li");
-
-  li.innerText = inputValue;
-
+  const li = createListItem();
   list.appendChild(li);
 
   listInput.value = "";
+}
+
+function createListItem() {
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click", deleteItem);
+  li.innerText = listInput.value;
+  li.appendChild(deleteButton);
+
+  return li;
 }
 
 addButton.addEventListener("click", addItem);
